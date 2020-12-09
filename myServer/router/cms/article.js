e8c601c8e0bccfdc99bc8d40a7cdec48be329b30
晏群 <cnyanqun@qq.com>
2020-12-09 17:00:45 +0800

    。

--- myServer/router/cms/article.js
+++ myServer/router/cms/article.js
@@ -1,8 +1,10 @@
-const { acticle_db, tag_db } = require('../mongo/index')
-
+const { acticle_db, tag_db } = require('../../mongo/index')
+const { sendData } = require('../../utils/sendData')
 
 module.exports = function (router) {
   router.post('/acticleAdd', (req, res) => {
     console.log(req.body)
+    let arr = ['1111', '2212122']
+    res.send(sendData(200, { memu: arr }))
   })
 }
