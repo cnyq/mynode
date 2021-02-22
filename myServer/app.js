f270a10b1303050df941b2b514a6fa3438f369c0
晏群 <cnyanqun@qq.com>
2021-02-22 17:42:47 +0800

    。

--- myServer/app.js
+++ myServer/app.js
@@ -5,7 +5,7 @@ const path = require('path')
 const cookieParser = require('cookie-parser');
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 9527
 
 //自定义中间件
 const myMiddleware = require('./middleware')
