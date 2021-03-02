748cd1d8f04c4844b2ddb88b370351b86b2cd33f
晏群 <cnyanqun@qq.com>
2021-03-02 17:53:21 +0800

    .

--- myServer/router/cms/users.js
+++ myServer/router/cms/users.js
@@ -5,6 +5,7 @@ const { myVerify } = require('../../utils/jwt')
 function findUser(username, isAuth = 3) {
   return new Promise((res, rej) => {
     USER.find({ username: username }, (err, data) => {
+      console.log('data', data)
       if (err) return rej()
       if (!data.length) {
         res(false)
@@ -25,11 +26,11 @@ function authUser(token, auth) {
     myVerify(token, (err, data) => {
       username = data.username
     })
-    if(!username) return res(false)
+    if (!username) return res(false)
     findUser(username, auth).then(it => {
       if (it) {
         res(true)
-      }else{
+      } else {
         res(false)
       }
     })
@@ -117,12 +118,86 @@ module.exports = function (router) {
       .catch(e => res.status(500).send('server error.'))
   })
 
-  router.post('/userAdd',async (req, res) => {
+  router.post('/userAdd', async (req, res) => {
     let token = req.cookies['token'], isAuth = false
-    await authUser(token,1).then(it=>{
+    await authUser(token, 1).then(it => {
       isAuth = it
     })
-    console.log(isAuth)
-    if(!isAuth) return res.sendDataFtm(500,null,'权限不足')
+    if (!isAuth) return res.sendDataFtm(500, null, '权限不足')
+    let { username, password, auth_status } = req.body
+    findUser(username).then(data => {
+      if (data) {
+        res.sendDataFtm(200, { status: 0, hint: '该用户名已被注册' }, false)
+      } else {
+        if (auth_status == 1) return res.sendDataFtm(200, { status: 0, hint: '禁止超级管理员注册' }, false)
+        let _data = {
+          username: username,
+          password: crypto.md5(password),
+          create_time: (new Date() - 0),
+          auth_status: auth_status
+        }
+        new USER(_data).save().then(it => {
+          res.sendDataFtm(200, { status: 1, hint: '新增成功' })
+        }).catch(err => {
+          console.log(err)
+          res.sendDataFtm(500, null, '新增失败')
+        })
+      }
+    }).catch(err => {
+      res.sendDataFtm(500)
+    })
+  })
+  router.post('/userEdit', async (req, res) => {
+    let token = req.cookies['token'], isAuth = false
+    await authUser(token, 1).then(it => {
+      isAuth = it
+    })
+    if (!isAuth) return res.sendDataFtm(500, null, '权限不足')
+    let { username, password, auth_status } = req.body
+    findUser(username).then(data => {
+      if (!data) {
+        res.sendDataFtm(200, { status: 0, hint: '未找到该用户' }, false)
+      } else {
+        let id = data[0]._id
+        if (auth_status == 1) return res.sendDataFtm(200, { status: 0, hint: '禁止超级管理员更改失败' }, false)
+        let _data = {
+          password: crypto.md5(password),
+          auth_status: parseInt(auth_status)
+        }
+        // data[0].password = crypto.md5(password)
+        // data[0].auth_status = parseInt(auth_status)
+        // data[0].save()
+        console.log('_data', _data)
+        USER.updateOne({ _id: id }, _data, (err, data) => {
+          if (err) return res.status(500).send('server error.')
+          console.log(data)
+          res.sendDataFtm(200, { status: 1, hint: '编辑成功' })
+        })
+      }
+    }).catch(err => {
+      res.sendDataFtm(500)
+    })
+  })
+  router.post('/userDel', async (req, res) => {
+    let token = req.cookies['token'], isAuth = false
+    await authUser(token, 1).then(it => {
+      isAuth = it
+    })
+    if (!isAuth) return res.sendDataFtm(500, null, '权限不足')
+    let { username, auth_status } = req.body
+    findUser(username).then(data => {
+      if (!data) {
+        res.sendDataFtm(200, { status: 0, hint: '未找到该用户' }, false)
+      } else {
+        let id = data[0]._id
+        if (auth_status == 1) return res.sendDataFtm(200, { status: 0, hint: '禁止超级管理员禁止删除' }, false)
+        USER.findByIdAndDelete(id).exec((err, doc) => {
+          if (err) return res.status(500).send('server error.')
+          res.sendDataFtm(200, { status: 1, hint: '删除成功' }, false)
+        })
+      }
+    }).catch(err => {
+      res.sendDataFtm(500)
+    })
   })
 }
